afa0abea490b81035a10955f1a26963381e328f2
Gabriel Sousa Leal <gabrielsousaleal@hotmail.com>
2021-02-22 22:45:22 -0300

    implementando a searchView

--- app/src/main/java/com/example/movies/MovieList/Activities/MovieList.kt
+++ app/src/main/java/com/example/movies/MovieList/Activities/MovieList.kt
@@ -3,6 +3,8 @@ package com.example.movies.MovieList.Activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.movies.Commons.Models.Movie
 import com.example.movies.MovieList.Adapter.MovieListAdapter
@@ -12,24 +14,43 @@ import kotlinx.android.synthetic.main.activity_movie_list.*
 
 class MovieList : AppCompatActivity() {
 
+    // MARK: - Private Properties
+
     private val viewModel = MovieListViewModel()
-    private var movieName = "suco"
+
+    // MARK: - Life Cicle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_list)
-
-        viewModel.getMoviesByName(movieName = movieName) { error, response, errorMessage ->
-            if (response != null) {
-                configureAdapter(movieList = response)
-            }
-        }
-
+        configureSearchView()
     }
 
-    fun configureAdapter(movieList: List<Movie>) {
+    // MARK: - Private Methods
+
+    private fun configureAdapter(movieList: List<Movie>) {
         val adapter = MovieListAdapter(movieList)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun searchMovies(movieName: String) {
+
+    }
+
+    private fun configureSearchView() {
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return false
+            }
+            override fun onQueryTextChange(newText: String): Boolean {
+                viewModel.getMoviesByName(movieName = newText) { error, response, errorMessage ->
+                    if (response != null) {
+                        configureAdapter(movieList = response)
+                    }
+                }
+                return false
+            }
+        })
     }
 }
